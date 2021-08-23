468cf5d49ed7ae79e5458e211f0f945b52e13e6d
zhaohongbing <1750427883@qq.com>
2021-08-23 14:40:14 +0800

    redis的简单使用

--- java_templates/src/test/java/com/zhao/java_templates/JavaTemplatesApplicationTests.java
+++ java_templates/src/test/java/com/zhao/java_templates/JavaTemplatesApplicationTests.java
@@ -1,13 +1,30 @@
 package com.zhao.java_templates;
 
+import com.zhao.java_templates.redis.RedisUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class JavaTemplatesApplicationTests {
 
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Resource
+    public RedisUtil redisUtil;
     @Test
     void contextLoads() {
+        ValueOperations vo = redisTemplate.opsForValue();
+        vo.set("test", "ceshi");
+        Object test = vo.get("test");
+        System.out.println(test);
+        redisUtil.set("hello","world");
+        Object hello = redisUtil.get("hello");
+        System.out.println(hello);
     }
 
 }
